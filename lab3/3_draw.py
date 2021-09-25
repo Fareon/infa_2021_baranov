58a7aa4eb7fd2417b20cd5a965aff1f6dfb373d5
Daniel <bdfaraon@gmail.com>
2021-09-25 11:35:08 +0300

    changed for PEP 8

--- lab3/3_draw.py
+++ lab3/3_draw.py
@@ -1,27 +1,28 @@
 import pygame
 from pygame.draw import *
 
+WHITE = (255, 255, 255)
+YELLOW = (255, 255, 51)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+
 pygame.init()
 
 FPS = 30
 screen = pygame.display.set_mode((700, 700))
-white = (255, 255, 255)
-screen.fill(white)
-yellow = (255, 255, 51)
-black = (0, 0, 0)
-red = (255, 0, 0)
+screen.fill(WHITE)
 
-pygame.draw.circle(screen, yellow, (350, 350), 100) #face
-pygame.draw.circle(screen, black, (350, 350), 100, 1) #face
-pygame.draw.circle(screen, red, (385, 320), 18)
-pygame.draw.circle(screen, black, (385, 320), 18, 1)        #right eye
-pygame.draw.circle(screen, black, (385, 320), 7)   #right eye center
-pygame.draw.circle(screen, red, (315, 320), 12)
-pygame.draw.circle(screen, black, (315, 320), 12, 1)  #left eye
-pygame.draw.circle(screen, black, (315, 320), 7)   #left eye center
-pygame.draw.line(screen, black, (310, 400), [390, 400], 15) #smile
-pygame.draw.polygon(screen, black, ((360, 315), (420, 275), (416, 269), (356, 309))) #right brow
-pygame.draw.polygon(screen, black, ((330, 316), (280, 276), (286, 270), (336, 310))) #left brow
+pygame.draw.circle(screen, YELLOW, (350, 350), 100) #face
+pygame.draw.circle(screen, BLACK, (350, 350), 100, 1) #face
+pygame.draw.circle(screen, RED, (385, 320), 18)
+pygame.draw.circle(screen, BLACK, (385, 320), 18, 1)        #right eye
+pygame.draw.circle(screen, BLACK, (385, 320), 7)   #right eye center
+pygame.draw.circle(screen, RED, (315, 320), 12)
+pygame.draw.circle(screen, BLACK, (315, 320), 12, 1)  #left eye
+pygame.draw.circle(screen, BLACK, (315, 320), 7)   #left eye center
+pygame.draw.line(screen, BLACK, (310, 400), [390, 400], 15) #smile
+pygame.draw.polygon(screen, BLACK, ((360, 315), (420, 275), (416, 269), (356, 309))) #right brow
+pygame.draw.polygon(screen, BLACK, ((330, 316), (280, 276), (286, 270), (336, 310))) #left brow
 
 
 
