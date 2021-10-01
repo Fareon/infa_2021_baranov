c47f50691eaf7910e819dfe2c30ed0f6899b7a68
valeryparfenov <parfenov.vr@phystech.edu>
2021-10-01 11:04:32 +0300

    инициализирована главная функция рисования lets_drow
    рисование фона переработано в функцию background

--- lab3/4_draw.py
+++ lab3/4_draw.py
@@ -3,8 +3,6 @@ from pygame.draw import *
 from random import randint
 from numpy import pi
 
-
-
 WHITE = (255, 255, 255)
 YELLOW = (255, 255, 51)
 BLACK = (0, 0, 0)
@@ -19,15 +17,17 @@ pygame.init()
 
 FPS = 30
 screen = pygame.display.set_mode((800, 800))
-screen.fill(LIGHT_BLUE)
 
-#background
-pygame.draw.line(screen, YELLOW, (0, 650), (800, 650), 300)
-pygame.draw.line(screen, BLUE, (0, 450), (800, 450), 150)
+
+def background(sand_height: int, sea_height: int):  # background
+    screen.fill(LIGHT_BLUE)  # sky
+    pygame.draw.line(screen, YELLOW, (0, 650), (800, 650), sand_height)  # sand
+    pygame.draw.line(screen, BLUE, (0, sand_height + sea_height), (800, sand_height + sea_height), sea_height)  # sea
+
 
 # clouds and sun
 for i in range(15):
-    cloud_x = randint(130,261)
+    cloud_x = randint(130, 261)
     cloud_y = randint(70, 201)
     pygame.draw.circle(screen, WHITE, (cloud_x, cloud_y), 25)
     pygame.draw.circle(screen, BLACK, (cloud_x, cloud_y), 25, 1)
@@ -41,7 +41,7 @@ for j in umbrella_lines:
     pygame.draw.line(screen, BLACK, (145, 430), (145 - j, 500), 1)
     pygame.draw.line(screen, BLACK, (155, 430), (155 + j, 500), 1)
 
-#ship
+# ship
 pygame.draw.line(screen, BROWN, (399, 425), (650, 425), 60)
 pygame.draw.polygon(screen, BROWN, [(650, 455), (650, 396), (750, 396)])
 for f in range(0, 61, 3):
@@ -54,18 +54,15 @@ pygame.draw.line(screen, BLACK, (490, 310), (555, 310), 1)
 pygame.draw.circle(screen, WHITE, (650, 425), 25)
 pygame.draw.circle(screen, BLACK, (650, 425), 25, 5)
 
-
-
-
-
-
-
+def lets_drow ():
+    background(300, 150)
+lets_drow()
 
 pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
 
-while not finished:
+while not finished:  # main cycle
     clock.tick(FPS)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
