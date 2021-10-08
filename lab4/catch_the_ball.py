a0d559bc8574655d75c81f9553c38736987b3928
Daniel <bdfaraon@gmail.com>
2021-10-08 18:40:59 +0300

    Started everything from the begining

---
+++ lab4/catch_the_ball.py
@@ -0,0 +1,30 @@
+import pygame
+from pygame.draw import *
+from random import randint
+pygame.init()
+
+FPS = 50
+screen = pygame.display.set_mode((1500, 800))
+
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+GREEN = (0, 255, 0)
+MAGENTA = (255, 0, 255)
+CYAN = (0, 255, 255)
+BLACK = (0, 0, 0)
+COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
+
+clock = pygame.time.Clock()
+finished = False
+
+while not finished:
+    clock.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            finished = True
+
+    pygame.display.update()
+
+
+pygame.quit()
