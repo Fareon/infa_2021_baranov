d10286564cf78c6cc1c1c121bfdc3a11f9faabad
Daniel <bdfaraon@gmail.com>
2021-10-08 19:03:52 +0300

    Defined some basic functions for drawing balls;

--- lab4/catch_the_ball.py
+++ lab4/catch_the_ball.py
@@ -3,8 +3,9 @@ from pygame.draw import *
 from random import randint
 pygame.init()
 
-FPS = 50
-screen = pygame.display.set_mode((1500, 800))
+FPS = 1
+SCREEN_WIDTH, SCREEN_HEIGHT = SPACE = (1500, 800)
+screen = pygame.display.set_mode(SPACE)
 
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
@@ -15,6 +16,37 @@ CYAN = (0, 255, 255)
 BLACK = (0, 0, 0)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 
+
+def gen_ball():
+    """
+    Generates parameters of a ball
+    :return: parameters for drawing a ball
+    """
+    ball_radius = randint(10, 100)
+    ball_x = randint(ball_radius, SCREEN_WIDTH - ball_radius)
+    ball_y = randint(ball_radius, SCREEN_HEIGHT - ball_radius)
+    ball_color = COLORS[randint(0, len(COLORS) - 1)]
+    return ball_color, ball_x, ball_y, ball_radius
+
+
+def draw_ball():
+    """
+    Draws a ball on the screen
+    :return:
+    """
+    for ball in balls:
+        circle(screen, ball[0], (ball[1], ball[2]), ball[3])
+
+
+def erase_ball():
+    """
+    Erases the ball from the screen
+    :return:
+    """
+
+
+balls = []
+
 clock = pygame.time.Clock()
 finished = False
 
@@ -23,7 +55,8 @@ while not finished:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finished = True
-
+    balls.append(gen_ball())
+    draw_ball()
     pygame.display.update()
 
 
