b4cce5d889aa67f917ef0b612a7039cdd2cbe9a4
Daniel <bdfaraon@gmail.com>
2021-10-09 18:00:58 +0300

    added another type of a ball, which gives 200 scores

--- lab4/catch_the_ball.py
+++ lab4/catch_the_ball.py
@@ -2,6 +2,7 @@ import pygame
 from pygame.draw import *
 from random import randint
 import numpy as np
+
 pygame.init()
 
 FPS = 30
@@ -19,6 +20,19 @@ BLACK = (0, 0, 0)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 
 
+def define_ball(possibility):
+    """
+    Defines which type of ball is to be generated
+    :param possibility: possibility of generating a super ball in percents
+    :return:
+    """
+    lotery = randint(1, int(100 / possibility))
+    if lotery % int(100 / possibility) == 0:
+        create_super_ball()
+    else:
+        create_ball()
+
+
 def gen_ball():
     """
     Generates parameters of a ball
@@ -31,13 +45,30 @@ def gen_ball():
     return [ball_color, ball_x, ball_y, ball_radius]
 
 
+def gen_super_ball():
+    """
+    Generates parameters of a super ball
+    :return: parameters for drawing a super ball
+    """
+    super_ball_radius = 80
+    super_ball_x = randint(super_ball_radius, SCREEN_WIDTH - super_ball_radius)
+    super_ball_y = randint(super_ball_radius, SCREEN_HEIGHT - super_ball_radius)
+    super_ball_color = (RED, BLUE, YELLOW, GREEN)
+    return [super_ball_color, super_ball_x, super_ball_y, super_ball_radius]
+
+
 def draw_balls():
     """
     Draws a ball on the screen
     :return:
     """
     for ball in balls:
-        circle(screen, ball[0], (ball[1], ball[2]), ball[3])  # Unpacking the tuple
+        circle(screen, ball[0], (ball[1], ball[2]), ball[3])  # Unpacking the list
+    for super_ball in super_balls:
+        circle(screen, super_ball[0][0], (super_ball[1], super_ball[2]), super_ball[3])  # Unpacking the list
+        circle(screen, super_ball[0][1], (super_ball[1], super_ball[2]), super_ball[3] - 20)
+        circle(screen, super_ball[0][2], (super_ball[1], super_ball[2]), super_ball[3] - 40)
+        circle(screen, super_ball[0][3], (super_ball[1], super_ball[2]), super_ball[3] - 60)
 
 
 def erase_definer(position):
@@ -53,32 +84,47 @@ def erase_definer(position):
     mouse_y = position[1]
     for ball in balls:
         if (mouse_x - ball[1]) ** 2 + (mouse_y - ball[2]) ** 2 <= ball[3] ** 2:  # Pifagor theorem
-            pop_ball(ball)
-            inner_counter += int(np.sqrt(ball[4] ** 2 + ball[5] ** 2) * (80 - ball[3])) + 1
+            balls.remove(ball)
+            inner_counter += (80 - ball[3]) + 1
+            define_ball(super_ball_possibility)
+    for super_ball in super_balls:
+        if (mouse_x - super_ball[1]) ** 2 + (mouse_y - super_ball[2]) ** 2 <= super_ball[3] ** 2:  # Pifagor theorem
+            super_balls.remove(super_ball)
+            inner_counter += 200
+            define_ball(super_ball_possibility)
     if inner_counter == 0:
-        inner_counter -= 10
+        inner_counter -= 20
     return inner_counter
 
 
-def pop_ball(ball):
+def create_ball():
     """
-    Erases the ball from the screen and creates a new one
+    Creates a new ball
     :return:
     """
-    balls.remove(ball)
     balls.append(gen_ball())
-    generate_velocity(-1)
+    generate_velocity(balls)
 
 
-def generate_velocity(ball_index):
+def create_super_ball():
+    """
+    Creates a new super ball
+    :return:
+    """
+    super_balls.append(gen_super_ball())
+    generate_velocity(super_balls)
+
+
+def generate_velocity(object_type, index=-1):
     """
     generates parameters for velocity of a ball
-    :param ball_index: index of a ball in a list of balls
+    :param object_type: defines which object will get a velocity
+    :param index: index of a ball in a list of balls
     :return:
     """
     horizontal_velocity, vertical_velocity = randint(-100, 100) / 100, randint(-100, 100) / 100
-    balls[ball_index].append(horizontal_velocity)
-    balls[ball_index].append(vertical_velocity)
+    object_type[index].append(horizontal_velocity)
+    object_type[index].append(vertical_velocity)
 
 
 def generate_velocity_all_balls():
@@ -87,10 +133,12 @@ def generate_velocity_all_balls():
     :return:
     """
     for ball in balls:
-        generate_velocity(balls.index(ball))
+        generate_velocity(balls, balls.index(ball))
+    for super_ball in super_balls:
+        generate_velocity(super_balls, super_balls.index(super_ball))
 
 
-def move_balls(times_moved=1):
+def move_balls(times_moved):
     """
     Moves every ball according their position and velocity
     :param times_moved: indicates the speed of time
@@ -110,8 +158,36 @@ def move_balls(times_moved=1):
             ball[2] += ball[5]  # adding velocity to ball_y
 
 
+def move_super_balls(times_moved):
+    """
+    Moves a super ball according its position and velocity
+    :param times_moved: indicates the speed of time
+    :return:
+    """
+    for super_ball in super_balls:
+        if super_ball[3] != 0:
+            super_ball[3] -= 1
+        else:
+            super_balls.remove(super_ball)
+            create_ball()
+    for time in range(times_moved):
+        for ball in super_balls:
+            # checking the position and changing horizontal velocity if needed
+            if ball[1] <= ball[3] + 1 or ball[1] >= SCREEN_WIDTH - (ball[3] + 1):
+                ball[4] *= -1
+
+            # checking the position and changing vertical velocity if needed
+            elif ball[2] <= ball[3] + 1 or ball[2] >= SCREEN_HEIGHT - (ball[3] + 1):
+                ball[5] *= -1
+
+            ball[1] += ball[4]  # adding velocity to ball_x
+            ball[2] += ball[5]  # adding velocity to ball_y
+
+
 number_of_balls = 5
 balls = [gen_ball() for i in range(number_of_balls)]
+super_balls = []
+super_ball_possibility = 20
 generate_velocity_all_balls()
 time_speed = 10
 scores = 0
@@ -133,6 +209,7 @@ while not finished:
 
     # Model
     move_balls(time_speed)
+    move_super_balls(time_speed * 2)
 
     # View
     screen.fill(BLACK)
@@ -140,6 +217,7 @@ while not finished:
     scores_counter = text.render(str(scores), True, GREY)
     screen.blit(scores_counter, scores_counter_position)
     draw_balls()
+
     pygame.display.update()
 
 
